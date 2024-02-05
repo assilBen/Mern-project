8ad8952c9c07219b237e6415f888e953b7cd713a
assil01 <assilbm0@gmail.com>
2024-02-05 10:01:41 +0100

    complete singUp page UI

--- client/src/pages/Singup.jsx
+++ client/src/pages/Singup.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 export default function Singup() {
   return (
-    <div>Singup</div>
-  )
+    <div className='p-3 max-w-lg mx-auto'>
+        <h1 className='text-3xl text-center font-semibold my-7'>Sing Up</h1>
+        <form className='flex flex-col gap-4'>
+        <input type="text" placeholder="Username" id="username" className='bg-slate-100 p-3 rounded-lg'></input>
+        <input type="text" placeholder="email" id="email" className='bg-slate-100 p-3 rounded-lg'></input>
+        <input type="text" placeholder="password" id="password" className='bg-slate-100 p-3 rounded-lg'></input>
+       <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sing up</button>
+        </form>
+        <div className='flex gap-2 mt-5'>
+            <p>Have an account?</p>
+            <Link to="/sing-in">
+            <span className='text-blue-500'>Sing in</span>
+            </Link>
+        </div>
+    </div>
+  );
 }
