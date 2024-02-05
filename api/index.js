c9bc224c0dcc96df3844552d6fadd44cd19bf314
assil01 <assilbm0@gmail.com>
2024-02-05 09:49:37 +0100

    add a middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import { stat } from 'fs';
 
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -20,3 +21,13 @@ app.listen(3000, ()=> {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || " Internel Server Error";
+    return res.status(statusCode).json({
+        sucess : false,
+        message,
+        statusCode,
+    });
+});
